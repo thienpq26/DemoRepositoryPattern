7a6b42c436cfa81d243179b4a713b3a6ed32168c
thien26 <pqthienhaui@gmail.com>
2020-04-21 16:46:49 +0700

    P3

--- app/src/main/java/com/example/demorepositorypattern/data/source/local/StudentLocalDataSource.kt
+++ app/src/main/java/com/example/demorepositorypattern/data/source/local/StudentLocalDataSource.kt
@@ -5,7 +5,25 @@ import com.example.demorepositorypattern.data.model.Student
 import com.example.demorepositorypattern.data.source.StudentDataSource
 import com.example.demorepositorypattern.data.source.local.dao.StudentDAO
 
-class StudentLocalDataSource(private var studentDAO: StudentDAO) {
+class StudentLocalDataSource(private var studentDAO: StudentDAO) : StudentDataSource.Local {
 
+    override fun getStudentAll(): ArrayList<Student> {
+        return studentDAO.getStudentAll()
+    }
 
+    override fun insertStudent(values: ContentValues): Long {
+        return studentDAO.insertStudent(values)
+    }
+
+    override fun updateStudent(
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<String>?
+    ): Int {
+        return studentDAO.updateStudent(values, selection, selectionArgs)
+    }
+
+    override fun deleteStudent(selection: String?, selectionArgs: Array<String>?): Int {
+        return studentDAO.deleteStudent(selection, selectionArgs)
+    }
 }
